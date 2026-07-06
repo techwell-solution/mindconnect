5f46612bf8812212c91f8fdc88f6730fe3c6ac2a
cataML <catelilungo@gmail.com>
2026-07-06 14:23:21 +0300

    feat: add case study page

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, about, services, service_detail
+from .views import home, about, services, service_detail, case_study
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('services/', services, name='services'),
     path("services/<slug:slug>/", service_detail, name="service_detail"),
+    path('case_study/', case_study, name='case_study'),
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -22,3 +22,6 @@ def services(request):
 def service_detail(request, slug):
     service = get_object_or_404(Services, slug=slug)
     return render(request, "core/service_detail.html", {"service": service})
+
+def case_study(request):
+    return render(request, 'core/case_study.html')

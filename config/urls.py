5028fe35eff1952647d433475f5388dbc181f4b0
cataML <catelilungo@gmail.com>
2026-07-07 12:51:32 +0300

    feat: add events page

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('events/', include('events.urls')),
 ]
 
 
